0047d7c7988d1528808b2472184d395d049537d9
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-12-22 11:03:13 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 birth_year=int(input("ENTER YEAR OF BIRTH :"))
 current_year=int(input("ENTER CURRENT YEAR :"))
 age=current_year-birth_year
-if(age>=20):
-    print ("YOUR AGE IS LESS THAN 20")
+if(age>=10)and(age<=20):
+         print ("YOUR AGE IS BETWEEN 10 AND 20")  
 else:
-    print("PRINT YOUR AGE IS GREATER THAN 20")
+ print ("YOUR AGE IS SMALLER THAN 10 OR GREATER THAN 20")  
